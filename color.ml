ded0535436a805a1466c16017b4819aa5e3b2448
Ben Levy <levyb@college.harvard.edu>
2020-02-25 14:28:35 -0500

    first

--- color.ml
+++ color.ml
@@ -10,7 +10,7 @@ doesn't pass all the unit tests. No need to debug it though. You'll be
 replacing it wholesale with a simpler implementation. *)
 
 (* 8-bit RGB channel colors *)
-type color = int ;;
+type color = (int*int*int) ;;
 
 (* Some standard color names *)
 type color_name =
@@ -24,11 +24,11 @@ type color_name =
 
 (* to_color r g b -- Returns the color corresponding to the RGB
    values given by r, g, and b *)
-let to_color (r : int) (g : int) (b : int) : color =
-  r lsl 0b10000 + g lsl 0b1000 + b ;;
+(* let to_color (r : int) (g : int) (b : int) : color =
+  r lsl 0b10000 + g lsl 0b1000 + b ;; *)
 
 (* red c -- Returns the red channel value for the color c *)
-let red (c : color) : int =
+(* let red (c : color) : int =
   c lsr 0b10000  ;;
 
 (* green c -- Returns the green channel value for the color c *)
@@ -37,16 +37,27 @@ let green (c : color) : int =
 
 (* blue c -- Returns the blue channel value for the color c *)
 let blue (c : color) : int =
-   c land 0b11111111 ;;
+   c land 0b11111111 ;; *)
 
 (* color_named name -- Returns the color (as RGB representation)
    corresponding to the color name *)
+
 let color_named (name : color_name) : color =
   match name with
-  | Red ->    0b111111110000000000000000
-  | Green ->  0b1111111100000000
-  | Blue ->   0b11111111
-  | Orange -> 0b111111111010110100000000
-  | Yellow -> 0b111111111111111000000000
-  | Indigo -> 0b010110110000000010000010
-  | Violet -> 0b111100001000001011110100 ;;
+  | Red ->    (255, 0, 0)
+  | Green ->  (0, 255, 0)
+  | Blue ->   (0, 0, 255)
+  | Orange -> (255, 165, 0)
+  | Yellow -> (255, 255, 0)
+  | Indigo -> (75, 0, 130)
+  | Violet -> (240, 130, 240) ;;
+
+let rgb_named (name : color_name) : color =
+  match name with
+  | (255, 0, 0) -> Red
+  | (0, 255, 0) -> Green
+  | (0, 0, 255) -> Blue
+  | (255, 165, 0) -> Orange
+  | (255, 255, 0) -> Yellow
+  | (75, 0, 130) -> Indigo
+  | (240, 130, 240) -> Violet ;;
